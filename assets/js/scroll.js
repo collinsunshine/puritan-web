c787e1c6754a9a41091aabad9e677caaee2afa62
Collin Sansom <collin.sansom@gmail.com>
2017-07-07 01:01:47 -0500

    new landing page

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,20 +1,20 @@
-    $(document).ready(function() {
-      // navigation click actions 
-      $('.scroll-link').on('click', function(event){
-        event.preventDefault();
-        var sectionID = $(this).attr("data-id");
-        scrollToID('#' + sectionID, 750);
-      });
-    });
-    // scroll function
-    function scrollToID(id, speed){
-      var offSet = 50;
-      var targetOffset = $(id).offset().top - offSet;
-      var mainNav = $('#main-nav');
-      $('html,body').animate({scrollTop:targetOffset}, speed);
-    }
-    if (typeof console === "undefined") {
-        console = {
-            log: function() { }
-        };
-    }
+$(document).ready(function() {
+  // navigation click actions 
+  $('.scroll-link').on('click', function(event){
+    event.preventDefault();
+    var sectionID = $(this).attr("data-id");
+    scrollToID('#' + sectionID, 1750);
+  });
+});
+// scroll function
+function scrollToID(id, speed){
+  var offSet = 00;
+  var targetOffset = $(id).offset().top - offSet;
+  var mainNav = $('#main-nav');
+  $('html,body').animate({scrollTop:targetOffset}, speed);
+}
+if (typeof console === "undefined") {
+    console = {
+        log: function() { }
+    };
+}
